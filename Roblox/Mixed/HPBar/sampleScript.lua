c47f66508cca13cc2540adde832969ccb2752b53
definedM <90419205+definedM@users.noreply.github.com>
2022-05-28 21:37:12 -0400

    inserted

--- Roblox/Mixed/HPBar/sampleScript.lua
+++ Roblox/Mixed/HPBar/sampleScript.lua
@@ -1,1 +1,16 @@
 
+
+local HPBarMod = require(game.ReplicatedStorage.HPBarModule)
+
+local posTab = {
+	['refreshOnRespawn'] = true,
+	['bypassUiFormat'] = false
+}
+for i = 1, 5 do
+HPBarMod.newBar(game.Players.LocalPlayer, posTab)
+end
+posTab['bypassUiFormat'] = true
+local barTab = HPBarMod.newBar(game.Players.LocalPlayer, posTab)
+local bar = barTab[1]
+bar.AnchorPoint = Vector2.new(1, 0)
+bar.Position = UDim2.new(0.9, 0, 0, 0)
